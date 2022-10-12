0f7806b018b274171fe7327d850598a395d4c02f
Leo DN <leodoan.081898@outlook.com>
2022-10-12 16:02:32 -0500

    TestProject

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+
+app.use("/public", express.static('assets'))
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname,'/assets/html/main.html'))})
 
 module.exports = router;
